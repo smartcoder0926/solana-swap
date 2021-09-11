4f025681205a827e86fa32aea48e34f16c6e0770
Dmytro Honcharov <fairyland0926@gmail.com>
2021-09-11 09:11:58 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import { deepPurple, pink } from '@material-ui/core/colors';
+import 'antd/dist/antd.dark.less';
+import { SnackbarProvider } from 'notistack';
+import React, { FC } from 'react';
+import './App.css';
+import { Dapp } from './Dapp';
+
+const theme = createTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: deepPurple[700],
+        },
+        secondary: {
+            main: pink[700],
+        },
+    },
+    overrides: {
+        MuiButtonBase: {
+            root: {
+                justifyContent: 'flex-start',
+            },
+        },
+        MuiButton: {
+            root: {
+                textTransform: undefined,
+                padding: '12px 16px',
+            },
+            startIcon: {
+                marginRight: 8,
+            },
+            endIcon: {
+                marginLeft: 8,
+            },
+        },
+    },
+});
+
+const App: FC = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <SnackbarProvider>
+                <Dapp />
+            </SnackbarProvider>
+        </ThemeProvider>
+    );
+};
+
+export default App;
